0b61d981b55c9f77f57ec792aac1ace8487b063e
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-22 22:52:07 -0500

    starting todos

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,75 @@
+
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// in mod plugins is in module.export 
+plugins: [
+  new InjectManifest({
+    swSrc: './src/sw.js',
+    swDest: 'service-worker.js',
+  }),
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    title: 'Webpack Plugin',
+  }),
+
+],
 // TODO: Add CSS loaders and babel to webpack.
+// module defined somewhere else?
+
+// module: {
+//   rules: [
+//     {
+//       test: /\.css$/i,
+//       use: ['style-loader', 'css-loader'],
+//     },
+//     {
+//       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//       type: 'asset/resource',
+//     },
+//     {
+//       test: /\.m?js$/,
+//       exclude: /(node_modules|bower_components)/,
+//       use: {
+//         loader: 'babel-loader',
+//         options: {
+//           presets: ['@babel/preset-env'],
+//         },
+//       },
+//     },
+//   ],
+// },
 
 module.exports = () => {
+
+
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i,
+  //       use: ['style-loader', 'css-loader'],
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  //       type: 'asset/resource',
+  //     },
+  //     {
+  //       test: /\.m?js$/,
+  //       exclude: /(node_modules|bower_components)/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['@babel/preset-env'],
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
+
   return {
     mode: 'development',
     entry: {
